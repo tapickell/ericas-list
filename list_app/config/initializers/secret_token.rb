8adaa30fdcadb11d29a68ca79d42a48e9afc39af
myappleguy <myappleguy@gmail.com>
2013-03-30 13:54:36 -0500

    getting setup

---
+++ list_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListApp::Application.config.secret_token = '41395e483998c97c8e901766845d987c8adfeff351ccdbacd1486c064f4e03fb8b6800b5c8d76bd4f8d832d1a1b0c40c1a01c527632063181b7d2a63e5d4164f'

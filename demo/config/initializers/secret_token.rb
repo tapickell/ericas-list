13c38410855e2e96959bc97b6a6202af3d7cef76
myappleguy <myappleguy@gmail.com>
2013-03-30 18:55:16 -0500

    wiped and started over version conflict unresolvable, could not run rake db:migrate on first migration for user model

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '9e735f03f58de1fcb7bb3b6c82ec3b3caad7daf811c79fb651bd0e4b2adeda3a70233609506e58032a78af7fc82502cc17e3453a5c159de38f915d3a809d5094'
